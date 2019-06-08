74a13740b3d39a271f46b2b2620d69cb5c17eba7
cloris111 <43761876+hlyy1994@users.noreply.github.com>
2019-06-08 17:25:45 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+#Assignment 2 for week 3, Lexical Scoping#
 
-makeCacheMatrix <- function(x = matrix()) {
 
+# This makeCacheMatrix function creates a special "matrix" object that can cache its inverse.
+makeCacheMatrix <- function(X = matrix()){
+    m <- NULL
+    set <- function(y){   #  set the value of the matrix
+        x <<- y 
+        m <<- NULL
+    }
+    get <- function() x  # get the value of the matrix
+    setInverse <- function(inverse) m <<- inverse  # set the value of inverse of the matrix
+    getInverse <- function() m  # get the value of inverse of the matrix
+    list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## Thxis cacheSolve function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
+## If the inverse has already been calculated (and the matrix has not changed) 
+##then the cachesolve should retrieve the inverse from the cache.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x,...){
+    m <- x$getInverse()
+    if(!is.null(m)){
+        message("getting cache data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data,...) ## If X is a square invertible matrix, then solve(X) returns its inverse.
+    x$setInverse(m)
+    m
 }
